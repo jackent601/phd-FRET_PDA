e29eb3f15784212923d0b7062f608d3750fcadf7
Jack <jackent601@gmail.com>
2023-02-19 04:20:58 +0000

    probability approach moved from KM2S to PDA file

--- PDA.py
+++ PDA.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import TwoStateKineticModel as KM2S
+import scipy
 
 # ================================================================================================================================
 #   PROBABILITY DISTRIBUTION ANALYSIS
@@ -13,6 +14,66 @@ import TwoStateKineticModel as KM2S
 
 # ================================================================================================================================
 
+
+# ================================================================================================================================
+# PROBABILITY APPROACH, AVOIDING MC SAMPLING
+# ================================================================================================================================
+
+# Described here: https://pubs.acs.org/doi/10.1021/jp102156t
+
+# ================================================================================================================================
+
+
+def getDiscrete_pEsn_forBurst(N, Ebins, F, burstDuration, k_1, k_minus1, E1, E2):
+
+    # Get p(T1) for this burst duration
+    T1s, T2s, PT1s = KM2S.PTi_TwoState_at_del_t(
+        N, burstDuration, k_1, k_minus1, cumsum=False, debug=False)
+
+    # get E snf corresponding to T1 values, note p(Esnf) = p(T1)
+    Esnf = KM2S.E_snf_TwoStatesEqualBrightness(T1s, E1, T2s, E2)
+    
+    # Get Binomial distribution for each Esnf_i
+    EsnfBinomials = scipy.stats.binom(F, Esnf) # Returns a generator for each Esnf_i at flourescence F
+    
+    # Also calculate the corresponding Esn for each potential value of of acceptor flourescence the 
+    # above distributions describe
+    Esn = np.array(range(0, F+1))/F
+    
+    # Calculates the p(Esn) for each Fr (Red photon) in F (at a particular Esnf_i) 
+    # Note where Fr > F p(Esn) = 0 so appropriate to loop in this manner
+    EsnBinomials = np.zeros((F+1, len(Esnf)))
+    for f in range(0, F+1):
+        EsnBinomials[f] = EsnfBinomials.pmf(f)
+
+    # returns an array where rows are Fg and columns are Esnf_i 
+    
+    # Now need to weight each Esn binomial with original likelyhood of Esnf (p(T1))
+    # Each column is a E_snf so matrix multiple by array of p(E_snf)
+    pWeightedEsn_i = EsnBinomials*PT1s
+    
+    # Sum each weighted Esn for final p(Esn)
+    pEsn = pWeightedEsn_i.sum(axis=1)
+    
+    # Finally get the binned probability to add together different bursts
+    discrete_pEsn, _, _ = scipy.stats.binned_statistic(Esn, pEsn, statistic='sum', bins=Ebins)
+    
+    return discrete_pEsn
+
+def getDiscrete_pEsn_forMultipleBurst(N, Ebins, F, burstDurations, k_1, k_minus1, E1, E2):
+    # Can either sum as you go (save space? at cost of time?) or sum at end (save time? at cost of space?)
+    bursts_discrete_pEsn = []
+    for b, _F in zip(burstDurations, F):
+        bursts_discrete_pEsn.append(getDiscrete_pEsn_forBurst(N=N, 
+                                                              Ebins=Ebins, 
+                                                              F=_F,
+                                                              burstDuration=b,
+                                                              k_1=k_1,
+                                                              k_minus1=k_minus1,
+                                                              E1=E1,
+                                                              E2=E2))
+    discrete_pEsn = np.array(bursts_discrete_pEsn).sum(axis=0)
+    return discrete_pEsn
 
 # ================================================================================================================================
 # HIGH RESOLUTION SEARCHING (BASED ON INDIVIDUAL BURST DURATIONS)
