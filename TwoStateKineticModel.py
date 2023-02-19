c01359be8f72fbb63a870a0027f89abdffdc5461
Jack <jackent601@gmail.com>
2023-02-19 04:18:37 +0000

    old methods removed

--- TwoStateKineticModel.py
+++ TwoStateKineticModel.py
@@ -86,49 +86,6 @@ def PTi_TwoState_at_del_t(N, burstDuration, k_1, k_minus1, cumsum = True, debug 
             
     return _T1s, _T2s, _PT1s
 
-def OLD_PTi_TwoState_at_del_t(N, burstDuration, k_1, k_minus1, cumsum = True, debug = False):
-    """
-    For given burst duration, k_1, k_minus1 calculates the probability distribution of spending t seconds in state 1 and state 2
-    Returns a dataframe of probability distribution for delt
-    """
-    
-    del_T = burstDuration/N
-    
-    P_T1s = []
-    
-    for n in range(N+1):
-
-        if n == 0:
-            # Primarily in State E2
-            _T1 = del_T/4
-            _T2 = burstDuration - _T1
-            _P_T1 = P_TwoState_T1_0(k1=k_1, k2=k_minus1, del_t=burstDuration) + P_TwoState_T1(k1=k_1, k2=k_minus1, T1=_T1, T2=_T2, del_T=del_T/2)
-        elif n == N:
-            # Primarily in State E1
-            _T2 = del_T/4
-            _T1 = burstDuration - _T2
-            _P_T1 = P_TwoState_T2_0(k1=k_1, k2=k_minus1, del_t=burstDuration) + P_TwoState_T1(k1=k_1, k2=k_minus1, T1=_T1, T2=_T2, del_T=del_T/2)
-        else:
-            # Transition Between States
-            _T1 = n*del_T
-            _T2 = burstDuration - _T1
-            _P_T1 = P_TwoState_T1(k1=k_1, k2=k_minus1, T1=_T1, T2=_T2, del_T=del_T)
-
-        P_T1s.append({'T1':_T1, 'T2':_T2, 'PT1': _P_T1, 'i': n})
-                
-        if debug:
-            print(f'T1: {_T1*1e3:0.2f} ms, T2: {_T2*1e3:0.2f} ms, P_T1: {_P_T1}')
-        
-    if np.any(np.array([d['PT1'] for d in P_T1s]) < 0):
-            print("WARNING NEGATIVE PROBABILITIES ENCOUNTERED")
-        
-    P_T1s_df = pd.DataFrame(P_T1s)
-    
-    if cumsum:
-        P_T1s_df['cumsum'] = P_T1s_df.PT1.cumsum()
-            
-    return P_T1s_df
-
 # ================================================================================================================================
 # CUMULATIVE FREQUENCY DISTRIBUTION GENERATION AND SAMPLING
 # ================================================================================================================================
@@ -179,16 +136,6 @@ def getMultipleT1T2SamplesFromBurstDuration(numberOfSamples, burstDuration, N, k
     _T1s, _T2s, _PT1s, _cumsum = PTi_TwoState_at_del_t(N=N, burstDuration=burstDuration, k_1=k_1, k_minus1=k_minus1, cumsum=True, debug=False)
     return getMultipleT1T2SamplesFromTiCFD(numberOfSamples, burstDuration, T_Vals=_T1s, T_CFD=_cumsum, seed=seed)
 
-def OLD_getMultipleT1T2SamplesFromBurstDuration(numberOfSamples, burstDuration, N, k_1, k_minus1, seed=None):
-    """
-    Generates a CFD from a Time Duration (and k, N values), then uses getMultipleT1T2SampleFromTiCFD to sample from it
-    """
-    # Generate T1 Distribution Based on this Duration (generation of CFD is limiting step)
-    P_T1_distributionForBurstLength = PTi_TwoState_at_del_t(N=N, burstDuration=burstDuration, k_1=k_1, k_minus1=k_minus1, cumsum=True, debug=False)
-    P_T1s = np.array(P_T1_distributionForBurstLength['T1'])
-    P_T1_cumsum = np.array(P_T1_distributionForBurstLength['cumsum'])
-    return getMultipleT1T2SamplesFromTiCFD(numberOfSamples, burstDuration, T_Vals=P_T1s, T_CFD=P_T1_cumsum, seed=seed)
-
 def p_getMultipleT1T2SamplesFromBurstDuration(numberOfSamples, burstDuration, PTwoStateParameters, seed=None):
     """
     Identical to getMultipleT1T2SamplesFromBurstDuration but N, k_1, k_minus1 provided in a parameter dictionary to tidy code
@@ -221,30 +168,6 @@ def p_getT1T2SamplesFromMultipleBurstDurations(durations, K, PTwoStateParameters
     # Get T1, T2 samples
     for _d in durations:
         T_pairs = getMultipleT1T2SamplesFromBurstDuration(K, _d, N, k_1, k_minus1, seed=seed)
-        Tpair_samples.append(T_pairs)
-
-    # Get arrays of T1, and T2 for paralleled calculation
-    allT1s = np.array([Tpair[0] for TpairSample in Tpair_samples for Tpair in TpairSample])
-    allT2s = np.array([Tpair[1] for TpairSample in Tpair_samples for Tpair in TpairSample])
-    
-    return allT1s, allT2s
-
-def OLD_p_getT1T2SamplesFromMultipleBurstDurations(durations, K, PTwoStateParameters, seed=None):
-    """
-    Runs through each duration, generates a CFD, samples T1, T2 values from CFD
-    N, k_1, k_minus1 provided in a parameter dictionary to tidy code
-    Returns two numpy arrays of T1, T2 values
-    """
-    # Unpack Two State Kinetic Parameters
-    N = PTwoStateParameters['N']
-    k_1 = PTwoStateParameters['k_1']
-    k_minus1 = PTwoStateParameters['k_minus1']
-
-    Tpair_samples = []
-
-    # Get T1, T2 samples
-    for _d in durations:
-        T_pairs = OLD_getMultipleT1T2SamplesFromBurstDuration(K, _d, N, k_1, k_minus1, seed=seed)
         Tpair_samples.append(T_pairs)
 
     # Get arrays of T1, and T2 for paralleled calculation
