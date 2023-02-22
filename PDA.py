0abf310ea9dfd35d9ad842e900220eaffd8c7dbf
Jack <jackent601@gmail.com>
2023-02-22 09:33:51 +0000

    fast search for pEsn, bins durations and averages Flourescent values

--- PDA.py
+++ PDA.py
@@ -24,11 +24,11 @@ import scipy
 # ================================================================================================================================
 
 
-def getDiscrete_pEsn_forBurst(N, Ebins, F, burstDuration, k_1, k_minus1, E1, E2):
+def getDiscrete_pEsn_forBurst(N, Ebins, F, burstDuration, k1, kminus1, E1, E2):
 
     # Get p(T1) for this burst duration
     T1s, T2s, PT1s = KM2S.PTi_TwoState_at_del_t(
-        N, burstDuration, k_1, k_minus1, cumsum=False, debug=False)
+        N, burstDuration, k1, kminus1, cumsum=False, debug=False)
 
     # get E snf corresponding to T1 values, note p(Esnf) = p(T1)
     Esnf = KM2S.E_snf_TwoStatesEqualBrightness(T1s, E1, T2s, E2)
@@ -60,7 +60,7 @@ def getDiscrete_pEsn_forBurst(N, Ebins, F, burstDuration, k_1, k_minus1, E1, E2)
     
     return discrete_pEsn
 
-def getDiscrete_pEsn_forMultipleBurst(N, Ebins, F, burstDurations, k_1, k_minus1, E1, E2):
+def getDiscrete_pEsn_forMultipleBurst(N, Ebins, F, burstDurations, k1, kminus1, E1, E2):
     # Can either sum as you go (save space? at cost of time?) or sum at end (save time? at cost of space?)
     bursts_discrete_pEsn = []
     for b, _F in zip(burstDurations, F):
@@ -68,8 +68,8 @@ def getDiscrete_pEsn_forMultipleBurst(N, Ebins, F, burstDurations, k_1, k_minus1
                                                               Ebins=Ebins, 
                                                               F=_F,
                                                               burstDuration=b,
-                                                              k_1=k_1,
-                                                              k_minus1=k_minus1,
+                                                              k1=k1,
+                                                              kminus1=kminus1,
                                                               E1=E1,
                                                               E2=E2))
     discrete_pEsn = np.array(bursts_discrete_pEsn).sum(axis=0)
@@ -114,7 +114,43 @@ def getEsnfEsnFromBurstDataFrame(BurstData, E1, E2, K, N, k1, kminus1, seed=None
 
 def get_pEsn_fromBurstDataFrame(BurstData, Ebins, E1, E2, N, k1, kminus1):
     d, F = getDurationsAndFlourescence(BurstData)
-    return KM2S.getDiscrete_pEsn_forMultipleBurst(N, Ebins, F, d, k1, kminus1, E1, E2)
+    return getDiscrete_pEsn_forMultipleBurst(N, Ebins, F, d, k1, kminus1, E1, E2)
+
+def fastFindpEsn_fromBurstDataFrame(BurstData, nBurstBins, Ebins, E1, E2, N, k1, kminus1):
+    """
+    Fast find for rough searching, bins duration values and runs average t F within each bin scaling resulting
+    pEsn for number of bursts within that bin
+    """
+    # Get Durations and Flourescences
+    d, F = getDurationsAndFlourescence(BurstData)
+
+    # Bin duration values
+    tsBinned, tBins = np.histogram(d, bins=nBurstBins)
+    dtBin = tBins[1]-tBins[0]
+    tBinCentres = tBins[:-1] + dtBin/2
+
+    # Get mean flourescence for each bin (Justification in documentation, guassian broadening limits braodening anyway)
+    Fmeans, _, _ = scipy.stats.binned_statistic(x=d, values=F, statistic='mean', bins = tBins)
+    FmeansInts = np.array([int(F) if not np.isnan(F) else 0 for F in Fmeans])
+    
+    # Keep only non-zero bins
+    tsBinnedNonZero = tsBinned[tsBinned != 0]
+    tBinCentresNonZero = tBinCentres[tsBinned != 0]
+    FmeanIntsNonZero = FmeansInts[tsBinned != 0]
+
+    # Modified 'getDiscrete_pEsn_forMultipleBurst' to scale for counts within each bin
+    bursts_discrete_pEsn = []
+    for b, W, _F in zip(tBinCentresNonZero, tsBinnedNonZero, FmeanIntsNonZero):
+        bursts_discrete_pEsn.append(W*getDiscrete_pEsn_forBurst(N=N,
+                                                                Ebins=Ebins,
+                                                                F=_F,
+                                                                burstDuration=b,
+                                                                k1=k1,
+                                                                kminus1=kminus1,
+                                                                E1=E1,
+                                                                E2=E2))
+    discrete_pEsn = np.array(bursts_discrete_pEsn).sum(axis=0)
+    return discrete_pEsn
 
 # ================================================================================================================================
 # SIMULATED E UTILITIES
@@ -142,4 +178,7 @@ def getSSEFromListOfEs(simulatedEs, Ebins, overSamplingFactorK, experimentEHist)
     simulated_E_Hist_Scaled = getScaledSimulatedEHist(simulatedEs, Ebins, overSamplingFactorK)
     # Return SSE
     return np.sum(np.array(np.array(experimentEHist) - np.array(simulated_E_Hist_Scaled))**2)
+
+def getSSEFromEHists(simulatedEHist, experimentEHist):
+    return np.sum(np.array(np.array(experimentEHist) - np.array(simulatedEHist))**2)
 
